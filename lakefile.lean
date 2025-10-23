176a945a48754ddfaf9c5f4c72c8c25d8354a4f0
pielet <syjia0325@gmail.com>
2025-10-23 15:39:00 +0800

    remove mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,5 +4,4 @@ open Lake DSL
 package "LeanPuzzles" where
   version := v!"0.1.0"
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 require autograder from git "https://github.com/rkthomps/lean4-autograder-main" @ "master"
